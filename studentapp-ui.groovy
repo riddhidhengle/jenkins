b50e5b6974fe52fedba32c9d3e7ca902f3ccff66
riddhidhengle <81424043+riddhidhengle@users.noreply.github.com>
2024-04-21 23:30:36 +0530

    Update studentapp-ui.groovy

--- studentapp-ui.groovy
+++ studentapp-ui.groovy
@@ -18,7 +18,7 @@ pipeline {
             steps {
                 sh '''/opt/apache-maven-3.9.6/bin/mvn sonar:sonar \\
                         -Dsonar.projectKey=student-ui \\
-                        -Dsonar.host.url=http://54.219.58.68:9000 \\
+                        -Dsonar.host.url=http://52.53.248.33:9000 \\
                         -Dsonar.login=283ab4fe2f00328ee0f57d591bba8484ebb244af'''
                 echo 'Testing done'
             }
